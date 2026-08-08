bd0d015222dfa76f5d91b91b4efeeb84fe1c5b6e
vivek <vvkpowerful2898@gmail.com>
2026-08-08 14:59:02 +0530

    Added gunicorn

--- extensions.py
+++ extensions.py
@@ -6,10 +6,12 @@ app.py, models, and routes (all of them import from here).
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
+mail = Mail()
 login_manager.login_view = "auth.login"
 login_manager.login_message = "Please log in to access this page."
 login_manager.login_message_category = "warning"
